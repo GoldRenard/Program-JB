437e693ec60aee4bf0e5351e0150f3ed159caeb5
Renard Gold <goldrenard@gmail.com>
2018-02-10 12:56:52 +0400

    initial fork

---
+++ src/test/java/org/alicebot/ab/InflectorTest.java
@@ -0,0 +1,21 @@
+package org.alicebot.ab;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by User on 3/31/14.
+ */
+public class InflectorTest extends TestCase {
+    @Test
+    public void testPluralize() throws Exception {
+        Inflector inflector = new Inflector();
+        String pairs[][] = {{"dog", "dogs"}, {"person", "people"}, {"cats", "cats"}};
+        for (int i = 0; i < pairs.length; i++) {
+            String singular = pairs[i][0];
+            String expected = pairs[i][1];
+            String actual = inflector.pluralize(singular);
+            assertEquals("Pluralize " + pairs[0][0], expected, actual);
+        }
+    }
+}
